91fa6dc338bd7aafe3d4bc7fef22e6cdd72e14b9
phoebemaifarrer <phoebemaifarrer@hotmail.co.uk>
2020-05-10 17:33:22 +0100

    New Feature Edit

--- Write To HTML.R
+++ Write To HTML.R
@@ -1,6 +1,6 @@
 
 library(knitr)
 library(rmarkdown)
-knit("Final_Project_Phoebe_Farrer.Rmd")
-render("Final_Project_Phoebe_Farrer.Rmd",
-       output_file = "Final_Project_Phoebe_Farrer.html")
+knit("Final_Project_Phoebe_Mai_Farrer.Rmd")
+render("Final_Project_Phoebe_Mai_Farrer.Rmd",
+       output_file = "Final_Project_Phoebe_Mai_Farrer.html")
